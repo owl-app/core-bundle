61beb510250207e58b90af5f91ce3d7bad66c61c
owl-app <owl@pawelkeska.eu>
2023-06-23 15:06:05 +0000

    Fixes

---
+++ Form/Type/Equipment/EquipmentAddOnChoiceType.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Owl\Bundle\CoreBundle\Form\Type\Equipment;
+
+use Owl\Component\Core\Model\EquipmentAddOnInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class EquipmentAddOnChoiceType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'choices' => [
+                EquipmentAddOnInterface::ADDON_REFUELING_NAME => EquipmentAddOnInterface::ADDON_REFUELING_CODE,
+                EquipmentAddOnInterface::ADDON_EVENT_NAME => EquipmentAddOnInterface::ADDON_EVENT_CODE
+            ],
+            'choice_translation_domain' => true,
+        ]);
+    }
+
+    public function getParent(): string
+    {
+        return ChoiceType::class;
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'owl_equipment_addon_choice';
+    }
+}
--- Form/Type/Equipment/EquipmentCategoryType.php
+++ Form/Type/Equipment/EquipmentCategoryType.php
@@ -4,10 +4,25 @@ declare(strict_types=1);
 
 namespace Owl\Bundle\CoreBundle\Form\Type\Equipment;
 
+use Symfony\Component\Form\FormBuilderInterface;
 use Owl\Bundle\CategoryBundle\Form\Type\CategoryType;
 
 final class EquipmentCategoryType extends CategoryType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add('addons', EquipmentAddOnChoiceType::class, [
+                'label' => 'owl.form.equipment.addon',
+                'multiple' => true,
+                'expanded' => false,
+                'required' => true,
+            ])
+        ;
+    }
+
     public function getBlockPrefix(): string
     {
         return 'owl_equipment_category';
